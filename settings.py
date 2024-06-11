87b93c67254be976f6d7b4422a0258722dffc2dc
CaraMc86 <150253604+CaraMc86@users.noreply.github.com>
2024-06-11 16:33:51 +0100

    adjusted the settings for core settings and synamic settings, also adding increase_speed method

--- settings.py
+++ settings.py
@@ -2,21 +2,17 @@ class Settings:
     """A class to store all settings for the Alien Invasion"""
 
     def __init__(self):
-        """Initialise the game settings to be used by other modules/main game"""
+        """Initialise the game static settings to be used by other modules/main game"""
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_colour = (162, 210, 255)
 
         # Ship settings
-        # Set the ship to an initial speed of 1.5 pixels, rather than 1 on each pass of hte loop
-        self.ship_speed = 1.5
         # Setting the number of ships/lives available
         self.ship_limit = 3
 
         # Bullet settings
-        # The bullet speed setting allows us to control the bullet speed as the game progresses
-        self.bullet_speed = 2.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_colour = (255, 175, 204)
@@ -24,9 +20,27 @@ class Settings:
         # This will then be used in the main game
         self.bullets_allowed = 3
 
-        # Alien settings
-        self.alien_speed = 1.0
         # Setting to control how quickly the fleet drops down
         self.fleet_drop_speed = 10
+
+        # Increment game speeds up by
+        self.speedup_scale = 1.1
+
+        self.initialise_dynamic_settings()
+
+    def initialise_dynamic_settings(self):
+        # Set the ship to an initial speed of 1.5 pixels, rather than 1 on each pass of hte loop
+        self.ship_speed = 1.5
+        # The bullet speed setting allows us to control the bullet speed as the game progresses
+        self.bullet_speed = 2.5
+        # Alien settings
+        self.alien_speed = 1.0
+
         # Fleet direction - 1 is right, -1 is left to match x-axis coordinate behaviour
         self.fleet_direction = 1
+
+    def increase_speed(self):
+        """Increase speed settings"""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
